5084dd197418292e6e0d976ff02907f31a4f5a4e
sheep <819574067@qq.com>
2013-09-03 22:55:25 +0800

    test

---
+++ io/InputReader.java
@@ -0,0 +1,45 @@
+package sheep.io;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * InputReader for fast IO operations
+ *
+ * @author sheepforever@gmail.com (Yang Xiao)
+ */
+public class InputReader {
+    BufferedReader reader;
+    StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+        reader = new BufferedReader(new InputStreamReader(stream));
+        tokenizer = null;
+    }
+
+    public String next() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (Exception e) {
+                throw new UnknownError();
+            }
+        }
+        return tokenizer.nextToken();
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    public double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+}
---
+++ util/ArrayUtils.java
@@ -0,0 +1,11 @@
+package sheep.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sheep
+ * Date: 13-9-3
+ * Time: 下午10:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArrayUtils {
+}
